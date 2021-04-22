3f89c24aeb246ab3fb2c029c5fed732d9d6b9eec
Shobith45 <shobith@robosoftin.com>
2021-04-22 21:27:41 +0530

    ui design commit on Apr 22

--- app/src/main/java/com/example/foursquareapplication/MainActivity.kt
+++ app/src/main/java/com/example/foursquareapplication/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.foursquareapplication
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inflater = layoutInflater.inflate(R.layout.rating_dialog,findViewById(R.id.main_root),false)
+        val dialog= AlertDialog.Builder(this).setView(inflater).create()
+        dialog.getWindow()?.setBackgroundDrawable( ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
 
     }
 }
