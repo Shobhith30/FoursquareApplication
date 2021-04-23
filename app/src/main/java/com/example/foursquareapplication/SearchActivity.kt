9f38d5a8810b21401c88dc3c4f7d3802508df39c
Navyashree311 <navyashree@robosoftin.com>
2021-04-23 17:27:33 +0530

    UI and activities new

---
+++ app/src/main/java/com/example/foursquareapplication/SearchActivity.kt
@@ -0,0 +1,38 @@
+package com.example.foursquareapplication
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_main)
+        supportActionBar?.hide()
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_search)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+         toolbar.setNavigationOnClickListener {
+         onBackPressed()
+         return@setNavigationOnClickListener
+    }
+
+        val fragmentSearchOptions = SearchOptionsFragment()
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+
+        transaction.add(R.id.search_fragment, fragmentSearchOptions)
+        transaction.commit()
+}
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_favourite, menu)
+        return super.onCreateOptionsMenu(menu)
+}
+}
