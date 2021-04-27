2e2aaa5c35765cf816cd2ecab5f50dbee29187e2
ramyakh1 <ramya.kh@robosoftin.com>
2021-04-27 15:06:19 +0530

    Ui design - Linking Screens

--- app/src/main/java/com/example/foursquareapplication/PhotosActivity.kt
+++ app/src/main/java/com/example/foursquareapplication/PhotosActivity.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.BaseAdapter
 import android.widget.GridView
 import android.widget.ImageView
