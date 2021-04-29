748b7443b248544128b2e7fd46c8d48f0083c01b
Shobith45 <shobith@robosoftin.com>
2021-04-29 07:45:48 +0530

    ui design commit - changes in FeatureAdapter.kt

--- app/src/main/java/com/example/foursquareapplication/FeatureAdapter.kt
+++ app/src/main/java/com/example/foursquareapplication/FeatureAdapter.kt
@@ -6,30 +6,38 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foursquareapplication.databinding.ItemFilterFeatureBinding
 
-class FeatureAdapter(private val featureList  : ArrayList<String>) : RecyclerView.Adapter<FeatureAdapter.FeatureViewHolder>()  {
+class FeatureAdapter(private val featureList: ArrayList<String>) :
+    RecyclerView.Adapter<FeatureAdapter.FeatureViewHolder>() {
 
-    inner class FeatureViewHolder(val featureBinding : ItemFilterFeatureBinding) : RecyclerView.ViewHolder(featureBinding.root){
+    inner class FeatureViewHolder(val featureBinding: ItemFilterFeatureBinding) :
+        RecyclerView.ViewHolder(featureBinding.root) {
         init {
-            featureBinding.addFeatureButton.setOnCheckedChangeListener { buttonView, isChecked ->
-                if(isChecked)
-                    featureBinding.featureName.setTextColor(Color.BLACK)
-                else
+            featureBinding.root.setOnClickListener {
+                if (featureBinding.addFeatureButton.isChecked) {
+
+                    featureBinding.addFeatureButton.isChecked = false
                     featureBinding.featureName.setTextColor(Color.parseColor("#8D8D8D"))
+
+                } else {
+                    featureBinding.addFeatureButton.isChecked = true
+                    featureBinding.featureName.setTextColor(Color.BLACK)
+
+                }
             }
+
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeatureViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val featureBinding = ItemFilterFeatureBinding.inflate(inflater,parent,false)
+        val featureBinding = ItemFilterFeatureBinding.inflate(inflater, parent, false)
         return FeatureViewHolder(featureBinding)
 
     }
 
     override fun onBindViewHolder(holder: FeatureViewHolder, position: Int) {
         holder.featureBinding.featureName.text = featureList[position]
-
 
 
     }
